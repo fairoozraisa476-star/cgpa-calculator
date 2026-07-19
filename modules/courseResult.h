4441b7cbecc46c5693d2c16a06348dbaa65d19b3
Fairooz Raisa <fairoozraisa476@gmail.com>
2026-07-20 05:12:19 +0600

    Declared semester course helpers

--- modules/courseResult.h
+++ modules/courseResult.h
@@ -1,15 +1,27 @@
 #ifndef COURSE_RESULT_H
- #define COURSE_RESULT_H
+#define COURSE_RESULT_H
 
- #include "course.h"
+#include "course.h"
 
- typedef struct CourseResult
+typedef struct CourseResult
 {
-Course *course;
- double marks;
- int completed;
- } CourseResult;
-CourseResult createCompletedCourseResult(Course *course, double marks);
-CourseResult createIncompleteCourseResult(Course *course);
+    Course *course;
+    double marks;
+} CourseResult;
+
+CourseResult createCourseResult(Course *course, double marks);
+
+void sortCourseResultsBySemester(CourseResult results[], int n_results);
+
+void filterCourseResultsBySemester(
+    CourseResult results[],
+    int n_results,
+    int semester,
+    CourseResult filtered[]
+);
+
+int countCourseResultsBeforeNull(CourseResult results[], int n_results);
+
 void viewCourseResult(CourseResult result);
+
 #endif
